2a612df057156b7e4b142f70ddfb9de26788c4ae
ilyass anida <anidailyass@gmail.com>
2024-10-29 12:45:16 +0100

    #7 Implement the ability to create a Task.

--- form.js
+++ form.js
@@ -1,106 +1,139 @@
-let startDateMenu = document.querySelector("#start-date-menu .dropdown-menu");
-let dueDateMenu = document.querySelector("#due-date-menu .dropdown-menu");
+function main() {
+    let startDateMenu = document.querySelector("#start-date-menu .dropdown-menu");
+    let dueDateMenu = document.querySelector("#due-date-menu .dropdown-menu");
 
-fillInputsWithDates();
+    fillInputsWithDates();
 
-const form = document.querySelector("form");
-form.addEventListener("submit", function (event){
-    event.preventDefault();
-    
-    let data = {
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value,
-        startDate: getValueFromMenu("start-date-menu"),
-        dueDate: getValueFromMenu("due-date-menu"),
-        priority: getValueFromMenu("priority-menu"),
-        state: getValueFromMenu("state-menu"),
-    }
+    const form = document.querySelector("form");
+    const emptyForm = form.cloneNode(true);
 
-    for (let key of Object.keys(data)) {
-        if(! data[key]){
-            document.querySelector("#alert").classList.remove("d-none");
-
-            document.querySelector("input").addEventListener("focus", hideAlert)
-            document.querySelector("textarea").addEventListener("focus", hideAlert)
-            document.querySelectorAll(".dropdown-toggle").forEach(function(ele){
-                ele.addEventListener("click", hideAlert)
-            })
-
-            function hideAlert() {
-                document.querySelector("#alert").classList.add("d-none");
-            }
-            return;
-        }
-    }
-
-    function getValueFromMenu (id){
-        let element = document.querySelector(`#${id} .dropdown-toggle`);
-        return element.hasAttribute("value") ? element.textContent : false;
-    }
-})
-
-function fillInputsWithDates() {
-    // Get the current time
-    let currentDate = new Date();
-
-    // Generates the dates items
-    generateDates(startDateMenu, currentDate)
-    generateDates(dueDateMenu, currentDate)
-
-    // Regenerate the dates of due menu based on the selected start date
-    document.querySelector("#due-date-menu .dropdown-toggle").addEventListener("click", function (event){
-    let startMenuIndex = document.querySelector("#start-date-menu .dropdown-toggle").getAttribute("value");
-    if(startMenuIndex === null) return;
-    dueDateMenu.innerHTML = "";
-    generateDates(dueDateMenu, currentDate, parseInt(startMenuIndex));
-    })
-
-    // Regenerate the dates of due menu based on the selected start date
-    document.querySelectorAll("#start-date-menu .dropdown-item").forEach(function (item){
-        item.addEventListener("click", function (event){
-            let dueMenuBtn = document.querySelector("#due-date-menu .dropdown-toggle");
-            let startMenuIndex = document.querySelector("#start-date-menu .dropdown-toggle").getAttribute("value");
-            let dueMenuIndex = dueMenuBtn.getAttribute("value");
-            
-            if(dueMenuIndex === null || startMenuIndex === null || startMenuIndex  <= dueMenuIndex) return;
+    form.addEventListener("submit", onSubmit)
 
 
-            dueMenuBtn.textContent = "Due Date";
-            dueMenuBtn.removeAttribute("value");
-        })
-    })
-
-    // Generate the date items in the menu
-    function generateDates(dateMenu, currentDate, skip = 0) {
-        for (let i = 0 + skip; i < 100; i++) {
-            let date = new Date(currentDate);
-            date.setDate(date.getDate() + i);
-            date = date.toLocaleDateString('en-US', 
-                { month: 'short', day: 'numeric', year: 'numeric', timeZone: 'Africa/Casablanca' }
-            );
-
-            let item = document.createElement('button');
-            item.setAttribute("value", i);
-            item.classList.add("dropdown-item");
-            item.textContent = date;
-
-            dateMenu.append(item);
-        }
-
-        document.querySelectorAll(".dropdown-item").forEach(function (item) {
-            item.addEventListener('click', markMenuOptionAsSelected);
-        })
-    }
-    
-    // Show the selected date on the select button
-    function markMenuOptionAsSelected (event) {
+    function onSubmit (event){
         event.preventDefault();
-        let selectedItem = event.target;
-        let selectBtn = getSelectBtnFromItemMenu(event);
-        selectBtn.textContent = selectedItem.textContent;
-        selectBtn.setAttribute("value", selectedItem.getAttribute("value"))
+        
+        let data = getTaskData();
+
+        if (validatedData(data)) {
+            addTask(data);
+        }
+        return;
     }
-    function getSelectBtnFromItemMenu (event) {
-        return event.target.parentElement.parentElement.querySelector(".dropdown-toggle");
+
+
+
+    function getTaskData() {
+        let data = {
+            title: document.getElementById("title").value,
+            description: document.getElementById("description").value,
+            startDate: getValueFromMenu("start-date-menu").textContent,
+            dueDate: getValueFromMenu("due-date-menu").textContent,
+            priority: getValueFromMenu("priority-menu").textContent,
+            state: getValueFromMenu("state-menu").getAttribute("value"),
+        }
+        function getValueFromMenu (id){
+            let element = document.querySelector(`#${id} .dropdown-toggle`);
+            return element.hasAttribute("value") ? element : false;
+        }
+
+        return data;
+    }
+
+    function validatedData(data) {
+        for (let key of Object.keys(data)) {
+            if(! data[key]){
+                document.querySelector("#alert").classList.remove("d-none");
+
+                document.querySelector("input").addEventListener("focus", hideAlert)
+                document.querySelector("textarea").addEventListener("focus", hideAlert)
+                document.querySelectorAll(".dropdown-toggle").forEach(function(ele){
+                    ele.addEventListener("click", hideAlert)
+                })
+
+                function hideAlert() {
+                    document.querySelector("#alert").classList.add("d-none");
+                }
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function addTask(data) {
+        createTask(data);
+        form.replaceWith(emptyForm); // Close PopUp
+        emptyForm.parentElement.classList.add("d-none");
+        main();
+    }
+
+    function fillInputsWithDates() {
+        // Get the current time
+        let currentDate = new Date();
+
+        // Generates the dates items
+        generateDates(startDateMenu, currentDate)
+        generateDates(dueDateMenu, currentDate)
+
+        // Regenerate the dates of due menu based on the selected start date
+        document.querySelector("#due-date-menu .dropdown-toggle").addEventListener("click", function (event){
+            let startMenuIndex = document.querySelector("#start-date-menu .dropdown-toggle").getAttribute("value");
+            if(startMenuIndex === null) return;
+            dueDateMenu.innerHTML = "";
+            generateDates(dueDateMenu, currentDate, parseInt(startMenuIndex));
+        })
+
+        // Regenerate the dates of due menu based on the selected start date
+        document.querySelectorAll("#start-date-menu .dropdown-item").forEach(function (item){
+            item.addEventListener("click", function (event){
+                let dueMenuBtn = document.querySelector("#due-date-menu .dropdown-toggle");
+                let startMenuIndex = document.querySelector("#start-date-menu .dropdown-toggle").getAttribute("value");
+                let dueMenuIndex = dueMenuBtn.getAttribute("value");
+                
+                if(dueMenuIndex === null || startMenuIndex === null || startMenuIndex  <= dueMenuIndex) return;
+
+
+                dueMenuBtn.textContent = "Due Date";
+                dueMenuBtn.removeAttribute("value");
+            })
+        })
+
+        // Generate the date items in the menu
+        function generateDates(dateMenu, currentDate, skip = 0) {
+            for (let i = 0 + skip; i < 100; i++) {
+                let date = new Date(currentDate);
+                date.setDate(date.getDate() + i);
+                date = date.toLocaleDateString('en-US', 
+                    { month: 'short', day: 'numeric', year: 'numeric', timeZone: 'Africa/Casablanca' }
+                );
+
+                let item = document.createElement('button');
+                item.setAttribute("value", i);
+                item.classList.add("dropdown-item");
+                item.textContent = date;
+
+                dateMenu.append(item);
+            }
+
+            document.querySelectorAll(".dropdown-item").forEach(function (item) {
+                item.addEventListener('click', markMenuOptionAsSelected);
+            })
+        }
+        
+        // Show the selected date on the select button
+        function markMenuOptionAsSelected (event) {
+            event.preventDefault();
+            let selectedItem = event.target;
+            let selectBtn = getSelectBtnFromItemMenu(event);
+            selectBtn.textContent = selectedItem.textContent;
+            selectBtn.setAttribute("value", selectedItem.getAttribute("value"))
+        }
+        function getSelectBtnFromItemMenu (event) {
+            return event.target.parentElement.parentElement.querySelector(".dropdown-toggle");
+        }
     }
 }
+
+main();
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let addBtn = document.getElementById("add-task");
 let formPopup = document.getElementById("create-task-popup");
 
+updateListsTasksCount();
+
 addBtn.addEventListener("click", function () {
     formPopup.classList.remove("d-none");
 
@@ -12,3 +14,38 @@ addBtn.addEventListener("click", function () {
         }
     })
 })
+
+
+function createTask(data) {
+    let colors = {
+        P1: "danger",
+        P2: "secondary",
+        P3: "info",
+    };
+    let task = `<div
+                    data-start-date="${data.startDate}"
+                    data-due-date="${data.dueDate}"
+                    data-priority="${data.priority}"
+                    data-state="${data.state}"
+                    data-description="${data.description}"
+                    class="task border-${colors[data.priority]}"
+                >
+                    <h6 class="font-weight-light mb-3">${data.title}</h6>
+                    <div class="labels">
+                        <button class="btn btn-danger py-0">Delete</button>
+                        <button class="btn btn-warning py-0 text-white">Edit</button>
+                    </div>
+                </div>
+                `
+    let listOfTasks = document.querySelectorAll(".list")[data.state].querySelector(".tasks");
+
+    listOfTasks.innerHTML += task;
+    updateListsTasksCount();
+}
+
+function updateListsTasksCount () {
+    document.querySelectorAll(".list").forEach( function (list) {
+        let spanCount = list.querySelector(".count");
+        spanCount.textContent = list.querySelectorAll(".tasks .task").length;
+    })
+}
